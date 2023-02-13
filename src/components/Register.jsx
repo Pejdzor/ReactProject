760528824f23b5c04c1127a41e3aacc07c48d42b
Pejdzor <mikolajpajor@student.agh.edu.pl>
2023-02-13 16:14:02 +0100

    Login Layout

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {useState,useEffect} from "react";
+import "../styles/styleregister.scss"
 const RegisterComponent = () => {
     return (
         <div className={"register"}>
